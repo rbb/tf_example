d8fa09bac7bf4e8c58e343bb370392e314cec5cf
Russell Brinkmann <russell_brinkmann@trimble.com>
2019-03-21 15:33:44 -0600

    Add Keras example (tfk_example.py)
    - Sort of a duplicate of tfhub_example, but using only keras, and not
    TensorFlow_Hub.
    - Start a module, pred_anal for making it easier to map imagenet outputs to
    custom classification layer (dogs,cats) and analyzing the accuracy of
    inferences.

---
+++ pred_anal.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+# From https://www.learnopencv.com/keras-tutorial-fine-tuning-using-pre-trained-models/
+
+# organize imports
+#from __future__ import print_function
+import numpy as np
+#import h5py
+import os
+import io
+import json
+import time
+import pickle
+
+import tensorflow as tf
+#import keras
+
+# Gather imagenet labels into groups that match our training sets
+class label_types(object):
+    def __init__(self, class_indices=None):
+        self.d = {
+            # imagenet classes/labels
+            "cat": np.array([281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293]),
+            "dog": np.concatenate(np.array(range(151,269)), 275)
+        }
+        self.rev_map = {} 
+        if class_indices:
+            self.class_indices = class_indices
+            self.map_labels()
+        else:
+            self.class_indices = {}
+    def map_labels(self, class_indices=None):
+        if not class_indices:
+            class_indices = self.class_indices
+        elif not self.class_indices:
+            self.class_indices = class_indices
+            
+        for k,v in class_indices.items():
+            self.rev_map[v] = k
+                
+    def match(self, prediction, label_ind):
+        N = len(prediction)
+        matches = np.zeros(N)
+        for n in range(N):
+            p = prediction[n]
+            #print("len(p) = {}".format(len(p)))
+            l = int(label_ind[n])
+            %print("match n,p,l =" +str((n,p,l)))
+            meta_label_list = self.d[self.rev_map[l]] 
+            if p.shape == ():
+                retval = np.any(meta_label_list == p)
+            else:
+                retval = 0
+                for m in range(p.shape[0]):
+                    if np.any(meta_label_list == p[m]):
+                        retval = 1
+            #print("match {}= ".format(n) +str(retval))
+            if retval:
+                matches[n] = 1
+        return matches
+    def accuracy(self, prediction, label_ind):
+        matches = self.match(prediction, label_ind)
+        fraction = np.sum(matches) / float(len(prediction))
+        return fraction
+
+def get_imagenet_labels(path='ImageNetLabels.txt', url='https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt'):
+    imagenet_labels_path = tf.keras.utils.get_file(path, url)
+    imagenet_labels = np.array(open(labels_path).read().splitlines())
+
+def result_top(x):
+    return np.argmax(x, axis=-1)
+
+def result_top_k(x, k):
+    return np.argpartition(x, -k)[:,-k:]
+
+def print_failures():
+    #print('Failures:')
+    #for n in range(cats.shape[0]):
+    #    if cats[n] == 0.0:
+    #        print('Example # ' +str(n) +' classified as ' +str(imagenet_labels[result_top[n]]))
+ 
+
+def score(top_pred, meta_labels):
+    N = len(top_pred)
+    correct = 0
+    for n in range(N):
+        for key in meta_labels.keys:
+            if np.any(top_pred(n) == label_types[key]):
+                correct += 1
+    accuracy = correct / float(N)
+    return accuracy
---
+++ tfk_example.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+
+# From https://www.learnopencv.com/keras-tutorial-fine-tuning-using-pre-trained-models/
+
+# organize imports
+#from __future__ import print_function
+import numpy as np
+#import h5py
+import os
+import io
+import json
+#import pickle
+#import seaborn as sns
+import matplotlib.pyplot as plt
+import psutil
+import time
+import pickle
+
+#from sklearn.metrics import classification_report
+#from sklearn.model_selection import train_test_split
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.metrics import confusion_matrix
+import tensorflow as tf
+#import keras
+#from keras import models
+#from keras import layers
+#from keras import optimizers
+#from keras.preprocessing import image
+#from keras.models import Model
+#from keras.models import model_from_json
+#from keras.layers import Input
+#from keras.applications.mobilenet import MobileNet, preprocess_input
+import pred_anal
+
+#
+# load the user configs
+# 
+config = json.load(io.open('tfk_conf.json', 'r', encoding='utf-8-sig'))
+
+def ld_conf_path(f, config=config, opath="out_path"):
+    p = os.path.join(config[opath], config[f])
+    return p
+
+# config variables
+#weights       = config["weights"]
+#include_top   = config["include_top"]
+#train_path    = config["train_path"]
+test_path     = config["test_path"]
+seed          = config["seed"]
+#batch_size    = config["batch_size"]
+img_side_len  = config["img_side_len"]
+out_path      = config["out_path"]
+image_size = (img_side_len, img_side_len)
+image_size_c = (img_side_len, img_side_len, 3)
+batch_size = 5
+
+#
+# Load the pre-trained model (without top layer)
+#
+#model = tf.keras.applications.mobilenet_v2.MobileNet(include_top=True, weights='imagenet',
+model = tf.keras.applications.mobilenet.MobileNet(include_top=True, weights='imagenet',
+        input_tensor=tf.keras.layers.Input(shape=image_size_c),
+        input_shape=image_size_c)
+
+ 
+# Show a summary of the model. Check the number of trainable parameters
+#model.summary()
+print("[INFO] used mem: {}% - ".format(psutil.virtual_memory().percent) )
+
+ 
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+test_data = test_datagen.flow_from_directory(
+        test_path,
+        target_size=image_size,
+        batch_size=batch_size,
+        class_mode='sparse',
+        shuffle=True)
+N_test_labels = test_data.labels.max() +1
+
+# Compile the model
+model.compile(loss='categorical_crossentropy',
+              optimizer=tf.keras.optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+print("[INFO] model compiled, used mem: {}% - ".format(psutil.virtual_memory().percent) )
+
+# Test the model
+start = time.time()
+
+for td_batch, td_batch_label in test_data:
+  print("Image batch shape: ", td_batch.shape)
+  print("Label batch shape: ", td_batch_label.shape)
+  break
+
+results = model.predict(
+        td_batch,
+        batch_size=batch_size,
+        steps =1)
+end = time.time()
+delta = end - start
+print ("[INFO] evaluation completed in {0:.3f} seconds".format(delta))
+print("[INFO] model trained, used mem: {}% - ".format(psutil.virtual_memory().percent) )
+ 
+# Check Performance
+#
+results_top = np.argmax(results, axis=-1)
+K = 5
+L = 10
+results_topk = pred_anal.result_top_k(results, K)
+results_topl = pred_anal.result_top_k(results, L)
+#labels_batch = imagenet_labels[result_top]
+#print(result_top)
+#print('Top 1 labels: ' +str(labels_batch))
+
+
+print('Scoring Top...')
+lt = pred_anal.label_types(test_data.class_indices )
+accuracy = lt.accuracy( results_top, td_batch_label)
+print("Top 1 accuracy = {0:.3f}".format(accuracy))
+
+accuracy = lt.accuracy( results_topk, td_batch_label)
+print("Top {0} accuracy = {1:.3f}".format(K,accuracy))
+
+accuracy = lt.accuracy( results_topl, td_batch_label)
+print("Top {0} accuracy = {1:.3f}".format(L,accuracy))
+
+
+end = time.time()
+print('Elapsed time to classify ' +str(batch_size) +' images: ' +str(end-start))
+
