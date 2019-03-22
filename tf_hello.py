222d1923f7b3717fe46f8af1c372272ca4558f20
Russell Brinkmann <russell_brinkmann@trimble.com>
2019-03-22 16:42:01 -0600

    Add some TF info to tf_hello.py
    - Built with CUDA
    - Is GPU available
    - TensorFlow Version

--- tf_hello.py
+++ tf_hello.py
@@ -9,3 +9,8 @@ hello = tf.constant('Hello, TensorFlow!')
 
 sess = tf.Session()
 print(sess.run(hello))
+
+print("TF built with cuda: " +str(tf.test.is_built_with_cuda()))
+print("TF is GPU available: " +str(tf.test.is_gpu_available()))
+print("TF version: " +str(tf.__version__))
+
