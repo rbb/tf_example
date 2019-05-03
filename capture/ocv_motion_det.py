436556959fd0ee296365617a1711b78f7cbd524a
Russell Brinkmann <rbb@users.noreply.github.com>
2019-05-02 20:55:15 -0900

    Working local save of 'occupied' frames

--- capture/ocv_motion_det.py
+++ capture/ocv_motion_det.py
@@ -15,6 +15,36 @@ import json
 import time
 import cv2
 import io
+import os
+
+# From: https://stackoverflow.com/questions/17984809/how-do-i-create-a-incrementing-filename-in-python
+def next_path(path_pattern):
+    """
+    Finds the next free path in an sequentially named list of files
+
+    e.g. path_pattern = 'file-%s.txt':
+
+    file-1.txt
+    file-2.txt
+    file-3.txt
+
+    Runs in log(n) time where n is the number of existing files in sequence
+    """
+    i = 1
+
+    # First do an exponential search
+    while os.path.exists(path_pattern % i):
+        i = i * 2
+
+    # Result lies somewhere in the interval (i/2..i]
+    # We call this interval (a..b] and narrow it down until a + 1 = b
+    a, b = (i / 2, i)
+    while a + 1 < b:
+        c = (a + b) / 2 # interval midpoint
+        a, b = (c, b) if os.path.exists(path_pattern % c) else (a, c)
+
+    return path_pattern % b
+
 
 
 # construct the argument parser and parse the arguments
@@ -29,9 +59,9 @@ ap.add_argument("--dropbox", action='store_true', default=False,
         help="Store frames to dropbox")
 ap.add_argument("--dropbox_base_path", default='ocv_motion_det/',
         help="path in dropbox folder for where to store frames: %(default)s")
-ap.add_argument("--min_upload_seconds", default=3.0,
+ap.add_argument("--min_upload_seconds", default=1.0,
         help="TODO: %(default)s")
-ap.add_argument("--min_motion_frames", default=4.0,
+ap.add_argument("--min_motion_frames", default=1,
         help="TODO: %(default)s")
 ap.add_argument("--camera_warmup_time", default=1.0,
         help="TODO: %(default)s seconds")
@@ -39,6 +69,8 @@ ap.add_argument("--delta_thresh", default=5,
         help="TODO: %(default)s")
 ap.add_argument("--min_area", default=5000,
         help="TODO: %(default)s")
+ap.add_argument("--out_dir", default="./frames/",
+        help="Where to store output frames (those deemed occupied): %(default)s")
 
 # Camera args
 ap.add_argument("--fps", default=16,
@@ -62,11 +94,16 @@ client = None
 
 # check to see if the Dropbox should be used
 if args.dropbox:
-    from pyimagesearch.tempimage import TempImage
+    #from pyimagesearch.tempimage import TempImage
+    from tempimage import TempImage
     import dropbox
     # connect to dropbox and start the session authorization process
     client = dropbox.Dropbox(conf["dropbox_access_token"])
     print("[SUCCESS] dropbox account linked")
+
+if not os.path.isdir(args.out_dir):
+    print("[INFO] creating directory " +str(args.out_dir))
+    os.makedirs(args.out_dir)
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -146,29 +183,32 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
         # check to see if the room is occupied
         if text == "Occupied":
-          print("[INFO] Occupied.")
-          # check to see if enough time has passed between uploads
-          if (timestamp - lastUploaded).seconds >= args.min_upload_seconds:
-              # increment the motion counter
-             motionCounter += 1
+            print("[INFO] Occupied.")
+            # check to see if enough time has passed between uploads
+            if (timestamp - lastUploaded).seconds >= args.min_upload_seconds:
+                # increment the motion counter
+                motionCounter += 1
 
-             # check to see if the number of frames with consistent motion is
-             # high enough
-             if motionCounter >= args.min_motion_frames:
-                 # check to see if dropbox sohuld be used
-                if args.dropbox:
-                    # write the image to temporary file
-                   t = TempImage()
-                   cv2.imwrite(t.path, frame)
+                # check to see if the number of frames with consistent motion is
+                # high enough
+                if motionCounter >= args.min_motion_frames:
+                    print("[INFO] found motion")
+                    # check to see if dropbox sohuld be used
+                    if args.dropbox:
+                        # write the image to temporary file
+                        t = TempImage()
+                        cv2.imwrite(t.path, frame)
 
-                   # upload the image to Dropbox and cleanup the tempory image
-                   print("[UPLOAD] {}".format(ts))
-                   path = "/{base_path}/{timestamp}.jpg".format(
-                           base_path=args.dropbox_base_path, timestamp=ts)
-                   client.files_upload(open(t.path, "rb").read(), path)
-                   t.cleanup()
-                #else:
-                # TODO: cv2.imwrite(t.path, frame)
+                        # upload the image to Dropbox and cleanup the tempory image
+                        print("[UPLOAD] {}".format(ts))
+                        path = "/{base_path}/{timestamp}.jpg".format(
+                               base_path=args.dropbox_base_path, timestamp=ts)
+                        client.files_upload(open(t.path, "rb").read(), path)
+                        t.cleanup()
+                    else:
+                        fn = next_path(os.path.join(args.out_dir, 'frame-%s.jpg'))
+                        print("[INFO] writing frame " +str(fn))
+                        cv2.imwrite(fn, frame)
 
                 # update the last uploaded timestamp and reset the motion
                 # counter
